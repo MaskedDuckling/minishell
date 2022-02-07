73a9680bddd66fa6bd87e14c9b885c16dd02dbfa
eydupray <eydupray@student.42.fr>
2022-02-07 20:26:23 +0100

    caca

--- Srcs/exec_command.c
+++ Srcs/exec_command.c
@@ -1,16 +1,24 @@
 #include "minishell.h"
 
+// cat Makefile | cat | wc
+
 void	child_process(t_command command, int *tube, int fd)
 {
-	close(tube[1]);
-	(void)fd;
+	char	*path;
+
 	if (fd != STDIN_FILENO)
+	{
 		dup2(fd, STDIN_FILENO);
-	if (tube[0] != STDOUT_FILENO)
-		dup2(tube[0], STDOUT_FILENO);
-	close(tube[0]);
-	command.argv[0] = for_access(command.argv[0], join_envi(command.envi));
-	execve(command.argv[0], command.argv, join_envi(command.envi));
+		close(fd);
+	}
+	if (tube[1] != STDOUT_FILENO)
+	{
+		dup2(tube[1], STDOUT_FILENO);
+		close(tube[1]);
+	}
+	path = for_access(command.argv[0], join_envi(command.envi));
+	execve(path, command.argv, join_envi(command.envi));
+	exit(1);
 }
 
 void	exec_command(t_command *commands)
@@ -26,13 +34,22 @@ void	exec_command(t_command *commands)
 	{
 		if (commands[i + 1].argv)
 			pipe(tube);
+		else
+			tube[1] = STDOUT_FILENO;
 		pid = fork();
 		if (pid == 0)
 			child_process(commands[i], tube, fd);
-		fd = tube[1];
+		if (i > 0)
+			close(fd);
+		fd = tube[0];
+		if (commands[i + 1].argv)
+			close(tube[1]);
 		i++;
 	}
-	waitpid(pid, 0, 0);
-	close(tube[0]);
-	close(tube[1]);
+	printf("a\n");
+	while (i >= 0)
+	{
+		pid = waitpid(-1, 0, 0);
+		i--;
+	}
 }
